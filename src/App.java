8567680316fed9fc01066d11401d85bd36802030
yujin gwak <ggthereisjin@gmail.com>
2025-03-25 10:46:49 +0900

    yujin gwak

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("안녕하세요 유진이입니다.");
+        System.out.println("안녕하세요 유진이입니다.22");
     }
 }
 
