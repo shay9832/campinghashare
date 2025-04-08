92d37eed70a7ab501504d5bd8073fefcc57ae6ba
Eunsol Lee <shay9832@gmail.com>
2025-04-08 18:38:27 +0900

    250408_DB 연결 구조 통일

---
+++ src/main/java/com/team/mvc/DTO/AdminMatchingListDTO.java
@@ -0,0 +1,12 @@
+package com.team.mvc.DTO;
+
+import java.util.Date;
+
+public class AdminMatchingListDTO {
+
+    //렌탈ID,렌탈제목,장비코드,장비명,소유자코드,신청자코드,렌탈_시작일,렌탈_종료일,매칭신청일,매칭상태,승인일자
+
+    private int rentalID;
+    private String rentalName,equipCode,equipName,ownerCode,applicantCode,matchingCondition;
+    private Date rentalStartDate,rentalEndDate,matchingStartDate,completeDate;
+}
---
+++ src/main/java/com/team/mvc/Interface/IRegisterStorenDAO.java
@@ -0,0 +1,12 @@
+package com.team.mvc.Interface;
+
+import com.team.mvc.DTO.IRegisterStorenDTO;
+
+import java.util.ArrayList;
+
+public interface IRegisterStorenDAO {
+
+    /*select*/
+    public ArrayList<IRegisterStorenDTO> list();
+
+}
