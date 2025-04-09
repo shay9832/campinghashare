83edc7d24d43ddb7523a9ae5663e26dbaa0fa62a
huni <jehoon0508@naver.com>
2025-04-09 09:49:48 +0900

    0409

--- src/main/java/com/team/mvc/AdminMainDAO.java
+++
@@ -1,9 +0,0 @@
-package com.team.mvc;
-
-import java.util.Date;
-
-public interface AdminMainDAO {
-
-    /*select*/
-    public AdminMainDTO count();
-}
--- src/main/java/com/team/mvc/Interface/IAdminInspectListDAO.java
+++ src/main/java/com/team/mvc/Interface/IAdminInspectListDAO.java
@@ -1,10 +1,21 @@
 package com.team.mvc.Interface;
 
+import com.team.mvc.DTO.AdminInspectListDTO;
+
 import java.util.ArrayList;
 
 public interface IAdminInspectListDAO {
 
     /*select*/
 
+    // 입고반환
     public ArrayList<IAdminInspectListDAO> list();
+
+    //스토렌반환
+    public ArrayList<IAdminInspectListDAO> listr();
+
+
+    /*insert*/
+    //검수결과 처리
+    public int add(AdminInspectListDTO ai);
 }
