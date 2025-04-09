906c7b5fc1d8ab73087267aaf6d5486e346efaea
Youngsik yoon <dudtlr6512@naver.com>
2025-04-09 17:55:02 +0900

    0409

--- src/main/java/com/team/mvc/Interface/ICategoryDAO.java
+++ src/main/java/com/team/mvc/Interface/ICategoryDAO.java
@@ -9,14 +9,14 @@ import java.util.List;
 @Mapper
 public interface ICategoryDAO {
     // 대분류 카테고리 목록 조회
-    List<CategoryDTO> getMajorCategory();
+    List<CategoryDTO> listMajorCategory();
 
     // 중분류 카테고리 목록 조회
-    List<CategoryDTO> getMiddleCategory(@Param("parentCategoryId") int parentCategoryId);
+    List<CategoryDTO> listMiddleCategory(@Param("parentCategoryId") int parentCategoryId);
 
     // 카테고리 ID로 카테고리 정보 조회
-    List<CategoryDTO> getCategoryById(@Param("categoryId") int categoryId);
+    List<CategoryDTO> listCategoryById(@Param("categoryId") int categoryId);
 
     // 카테고리 이름으로 카테고리 정보 조회
-    List<CategoryDTO> getCategoryByName(@Param("categoryName") String categoryName);
+    List<CategoryDTO> listCategoryByName(@Param("categoryName") String categoryName);
 }
