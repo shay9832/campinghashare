dd87238f9484abe2f616730bbf761e772dd3d253
huni <jehoon0508@naver.com>
2025-04-21 09:28:45 +0900

    게시판 작업

--- src/main/java/com/team/mvc/Controller/AdminUserListController.java
+++ src/main/java/com/team/mvc/Controller/AdminUserListController.java
@@ -4,6 +4,7 @@ import com.team.mvc.Interface.IAdminUserListDAO;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,9 +15,11 @@ public class AdminUserListController {
     private SqlSession sqlSession;
 
     @RequestMapping(value="/admin-userList.action",method = RequestMethod.GET)
-    public String adminUserList(){
+    public String adminUserList(Model model){
 
-        IAdminUserListDAO adminUserListDAO = sqlSession.getMapper(IAdminUserListDAO.class);
+        IAdminUserListDAO dao = sqlSession.getMapper(IAdminUserListDAO.class);
+
+        model.addAttribute("getList",dao.getList());
 
 
         return "admin-userList";
