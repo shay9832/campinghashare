f0b081184d5e58e281878980c71db4f6b97ba3f6
Yujin Gwak <ggthereisjin@gmail.com>
2025-04-22 17:51:25 +0900

    0422 검색개선 및  내대장 완성

--- src/main/java/com/team/mvc/Interface/IMypageRentEquipService.java
+++ src/main/java/com/team/mvc/Interface/IMypageRentEquipService.java
@@ -2,8 +2,12 @@ package com.team.mvc.Interface;
 
 import com.team.mvc.DTO.MyRentEquipDTO;
 
+import java.util.Map;
+
 
 public interface IMypageRentEquipService {
     // 내가 대여한 장비 - 스토렌 탭에서 필요한 정보를 따로 정의한 상위 DTO로 반환하는 메소드
     MyRentEquipDTO listRentStorenInfo(int userCode);
+    // 장비 상태 조회
+    Map<String, Map<String, Integer>> getRentEquipmentStatus(int userCode);
 }
