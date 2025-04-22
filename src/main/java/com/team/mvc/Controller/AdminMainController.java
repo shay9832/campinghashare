569d25355e94fc8b656c89a44a04484ea7297a2c
huni <jehoon0508@naver.com>
2025-04-22 17:47:23 +0900

    0422 검수 오류해결 완료

--- src/main/java/com/team/mvc/Controller/AdminMainController.java
+++ src/main/java/com/team/mvc/Controller/AdminMainController.java
@@ -1,5 +1,7 @@
 package com.team.mvc.Controller;
 
+import com.team.mvc.DTO.AdminUserReportDTO;
+import com.team.mvc.Interface.IAdminAllService;
 import com.team.mvc.Interface.IAdminMainDAO;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,19 +10,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 @Controller
 public class AdminMainController {
 
     // 자동 의존성 주입
     @Autowired
-    private SqlSession sqlSession;
+    private IAdminAllService adminAllService;
 
     @RequestMapping(value = "/admin-main.action", method = RequestMethod.GET)
     public String adminMain(Model model) {
 
-        IAdminMainDAO dao = sqlSession.getMapper(IAdminMainDAO.class);
 
-        model.addAttribute("count",dao.count());
+        List<AdminUserReportDTO> reportDTO = adminAllService.getList();
+
+        model.addAttribute("report", reportDTO);
+
         return "admin-main";
     }
 }
--- src/main/java/com/team/mvc/Interface/IAdminAllService.java
+++ src/main/java/com/team/mvc/Interface/IAdminAllService.java
@@ -1,14 +1,13 @@
 package com.team.mvc.Interface;
 
 import com.team.mvc.DTO.AdminUserReportDTO;
-import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
-@Mapper
-public interface IAdminUserReportDAO {
+public interface IAdminAllService {
+
 
     List<AdminUserReportDTO> getList();
 
-    List<AdminUserReportDTO> getReportContent();
+
 }
