56b518f1683fa9a0ace9a619b8367dfdbca4cf7c
huni <jehoon0508@naver.com>
2025-04-23 12:16:43 +0900

    0423 쿠폰 리스트 작업 완료

--- src/main/java/com/team/mvc/Interface/IAdminCreateCouponDAO.java
+++ src/main/java/com/team/mvc/Interface/IAdminCreateCouponDAO.java
@@ -1,10 +1,12 @@
 package com.team.mvc.Interface;
 
 import com.team.mvc.DTO.AdminCreateCouponDTO;
+import java.util.List;
 
 public interface IAdminCreateCouponDAO {
+    // 쿠폰 타입 목록 조회
+    List<AdminCreateCouponDTO> getList();
 
-    /*insert*/
-    int setCoupon();
-
+    // 쿠폰 생성 - DTO 객체를 파라미터로 사용
+    void setCoupon(AdminCreateCouponDTO dto);
 }
